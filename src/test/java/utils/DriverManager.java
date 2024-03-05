c2b45350198f56e02fb72e3b553a1619b1022cc1
pterodont32 <pterodont32@gmail.com>
2024-03-05 18:27:31 +0200

    hello

--- src/test/java/utils/DriverManager.java
+++ src/test/java/utils/DriverManager.java
@@ -1,25 +1,45 @@
 package utils;
 
 
+import dataProviders.ConfigFileReader;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
 
-public class DriverManager {
-    private static WebDriver driver;
+import static utils.PropertyUtils.getPropertyFromConfigFile;
 
-    public static WebDriver getDriver() {
-        if (driver == null) {
-            // Initialize the WebDriver instance
-            System.setProperty("webdriver.chrome.driver", "C:/Users/Vlungu/IdeaProjects/ATF_Selenium/src/test/resources/driver/chromedriver.exe");
-            driver = new ChromeDriver();
-            driver.manage().window().maximize();
-            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-            driver.get("https://shop.demoqa.com/my-account/");
+//public class DriverManager {
+//    private static WebDriver driver;
+//
+//    public static WebDriver getDriver() {
+//        if (driver == null) {
+//            // Initialize the WebDriver instance
+//            System.setProperty("webdriver.chrome.driver", "C:/Users/Vlungu/IdeaProjects/ATF_Selenium/src/test/resources/driver/chromedriver.exe");
+//            driver = new ChromeDriver();
+//            driver.manage().window().maximize();
+//            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//            driver.get("https://shop.demoqa.com/my-account/");
+//        }
+//        return driver;
+//    }
+
+
+    public class DriverManager {
+        private static WebDriver driver;
+
+        public static WebDriver getDriver() {
+            if (driver == null) {
+                // Initialize the WebDriver instance
+                System.setProperty("webdriver.chrome.driver", getPropertyFromConfigFile("driverPath"));
+                driver = new ChromeDriver();
+                driver.manage().window().maximize();
+                driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                driver.get("https://shop.demoqa.com/my-account/");
+            }
+            return driver;
         }
-        return driver;
-    }
+
 
     public static void closeDriver() {
         driver.quit();
@@ -30,30 +50,4 @@ public class DriverManager {
 
 
 
-//
-//package utils;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.chrome.ChromeDriver;
-//import java.util.concurrent.TimeUnit;
-//import dataProviders.ConfigFileReader;
-//
-//public class DriverManager {
-//    private static WebDriver driver;
-//    static ConfigFileReader configFileReader;
-//    public static WebDriver getDriver() {
-//        if (driver == null) {
-//            // Initialize the WebDriver instance
-//            System.setProperty("webdriver.chrome.driver",configFileReader.getDriverPath());
-//            driver = new ChromeDriver();
-//            driver.manage().window().maximize();
-//            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//            driver.get("https://shop.demoqa.com/my-account/");
-//        }
-//        return driver;
-//    }
-//}
 
-
-
-
-// info     https://www.toolsqa.com/selenium-cucumber-framework/read-configurations-from-property-file/
---
+++ src/test/java/utils/PropertyUtils.java
@@ -0,0 +1,14 @@
+package utils;
+
+import dataProviders.ConfigFileReader;
+
+import java.util.Properties;
+
+public class PropertyUtils {
+
+    public static String getPropertyFromConfigFile(String key) {
+        ConfigFileReader configFileReader = new ConfigFileReader();
+        Properties properties = configFileReader.initProperties();
+        return properties.getProperty(key);
+    }
+}
